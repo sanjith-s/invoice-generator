dd3ccc77cbfe06606aef1ce3febcaf555fcf1e40
V Ryan <ryan19vt@gmail.com>
2023-01-14 13:34:02 +0530

    Updated index.html and App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,7 @@
     addr2: "",
     city: "",
     zipcode: "",
-    gstin: "",
+    contact: "",
   };
 
   let billerDetails = {
@@ -26,7 +26,14 @@
     addr2: "",
     city: "",
     zipcode: "",
+    contact: "",
     gstin: "",
+  };
+
+  let invoiceDetails = {
+    number: "",
+    date: "",
+    term: "",
   };
 
   let itemObj = {
@@ -85,125 +92,80 @@
       <ImgBox />
     </div>
 
-    <div id="clientDetails" class="details">
-      <span class="title2">To</span>
-      <label
-        ><input
-          id="cname"
-          type="text"
-          placeholder="Name"
-          bind:value={clientDetails.name}
-        /></label
-      >
-      <label
-        ><input
-          id="cemail"
-          type="text"
-          placeholder="Email"
-          bind:value={clientDetails.email}
-        /></label
-      >
-      <label
-        ><input
-          id="caddr1"
-          type="text"
-          placeholder="Address line 1"
-          bind:value={clientDetails.addr1}
-        /></label
-      >
-      <label
-        ><input
-          id="caddr2"
-          type="text"
-          placeholder="Address line 2"
-          bind:value={clientDetails.addr2}
-        /></label
-      >
-      <label
-        ><input
-          id="ccity"
-          type="text"
-          placeholder="City"
-          bind:value={clientDetails.city}
-        /></label
-      >
-      <label
-        ><input
-          id="czip"
-          type="text"
-          placeholder="Pincode"
-          bind:value={clientDetails.zipcode}
-        /></label
-      >
-      <label
-        ><input
-          id="cgstin"
-          type="text"
-          placeholder="GSTIN"
-          bind:value={clientDetails.gstin}
-        /></label
-      >
+    <div class="grid-container">
+      <div id="billerDetails" class="details">
+        <span class="title2">From</span>
+        <label>
+          <input id="bname" type="text" placeholder="Name"
+            bind:value={billerDetails.name}
+          /></label>
+        <label>
+          <input id="bemail" type="text" placeholder="Email"
+            bind:value={billerDetails.email}
+          /></label>
+        <label><input id="baddr1" type="text" placeholder="Address line 1"
+            bind:value={billerDetails.addr1}
+          /></label>
+        <label><input id="baddr2" type="text" placeholder="Address line 2"
+            bind:value={billerDetails.addr2}
+          /></label>
+        <label><input id="bcity" type="text" placeholder="City"
+            bind:value={billerDetails.city}
+          /></label>
+        <label><input id="bzip" type="text" placeholder="Pincode"
+            bind:value={billerDetails.zipcode}
+          /></label>
+        <label><input id="bcontact" type="text" placeholder="Contact No"
+            bind:value={billerDetails.contact}
+          /></label>
+        <label><input id="bgstin" type="text" placeholder="GSTIN"
+            bind:value={billerDetails.gstin}
+          /></label>
+      </div>
+
+      <div id="clientDetails" class="details">
+        <span class="title2">To</span>
+        <label><input id="cname" type="text" placeholder="Name"
+            bind:value={clientDetails.name}
+          /></label>
+        <label><input id="cemail" type="text" placeholder="Email"
+            bind:value={clientDetails.email}
+          /></label>
+        <label><input id="caddr1" type="text" placeholder="Address line 1"
+            bind:value={clientDetails.addr1}
+          /></label>
+        <label><input id="caddr2" type="text" placeholder="Address line 2"
+            bind:value={clientDetails.addr2}
+          /></label>
+        <label><input id="ccity" type="text" placeholder="City"
+            bind:value={clientDetails.city}
+          /></label>
+        <label><input id="czip" type="text" placeholder="Pincode"
+            bind:value={clientDetails.zipcode}
+          /></label>
+        <label><input id="ccontact" type="text" placeholder="Contact No"
+            bind:value={clientDetails.contact}
+          /></label>
+      </div>
     </div>
 
-    <div id="billerDetails" class="details">
-      <span class="title2">From</span>
-      <label
-        ><input
-          id="bname"
-          type="text"
-          placeholder="Name"
-          bind:value={billerDetails.name}
-        /></label
-      >
-      <label
-        ><input
-          id="bemail"
-          type="text"
-          placeholder="Email"
-          bind:value={billerDetails.email}
-        /></label
-      >
-      <label
-        ><input
-          id="baddr1"
-          type="text"
-          placeholder="Address line 1"
-          bind:value={billerDetails.addr1}
-        /></label
-      >
-      <label
-        ><input
-          id="baddr2"
-          type="text"
-          placeholder="Address line 2"
-          bind:value={billerDetails.addr2}
-        /></label
-      >
-      <label
-        ><input
-          id="bcity"
-          type="text"
-          placeholder="City"
-          bind:value={billerDetails.city}
-        /></label
-      >
-      <label
-        ><input
-          id="bzip"
-          type="text"
-          placeholder="Pincode"
-          bind:value={billerDetails.zipcode}
-        /></label
-      >
-      <label
-        ><input
-          id="bgstin"
-          type="text"
-          placeholder="GSTIN"
-          bind:value={billerDetails.gstin}
-        /></label
-      >
+    <div>
+      <hr/>
+      <div id="invoiceDetails" class="details">
+        <span class="title2">Invoice Details</span>
+        <label><input id="number" type="text" placeholder="Invoice Number"
+          bind:value={invoiceDetails.number}
+        /></label>
+        <label><input id="date" type="date" 
+          bind:value={invoiceDetails.date}
+        /></label>
+        <label><input id="term" type="text" placeholder="Invoice Term"
+          bind:value={invoiceDetails.term}
+        /></label>
+      </div>
+      <hr/>
     </div>
+
     <div>
       <span class="title2">Items</span>
       {#each items as item, id}
@@ -211,9 +173,20 @@
         <hr />
       {/each}
     </div>
-    <button type="button" on:click={addItem}>Add Item</button>
+    <button type="button" class="add" on:click={addItem}>
+      <i class="fa-solid fa-plus" /> Add Item</button
+    >
   </div>
 </form>
+
+<!-- Iconss -->
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+  integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
@@ -242,6 +215,11 @@
     margin: auto;
   }
 
+  .grid-container {
+    display: grid;
+    grid-template-columns: 50% 50%;
+  }
+
   .details {
     display: flex;
     flex-direction: column;
@@ -267,6 +245,31 @@
   .title1 {
     font-size: 48px;
     font-weight: 600;
+  }
+
+  .add {
+    background: #0066ff;
+    border-radius: 999px;
+    box-shadow: #005de2 0 10px 20px -10px;
+    box-sizing: border-box;
+    color: #ffffff;
+    cursor: pointer;
+    font-family: Inter, Helvetica, "Apple Color Emoji", "Segoe UI Emoji",
+      NotoColorEmoji, "Noto Color Emoji", "Segoe UI Symbol", "Android Emoji",
+      EmojiSymbols, -apple-system, system-ui, "Segoe UI", Roboto,
+      "Helvetica Neue", "Noto Sans", sans-serif;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 24px;
+    opacity: 1;
+    outline: 0 solid transparent;
+    padding: 8px 18px;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    width: fit-content;
+    word-break: break-word;
+    border: 0;
   }
 
   #titleBox {
